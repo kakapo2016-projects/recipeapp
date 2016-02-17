f3b274a0c0e23e16589c81ca3034d6061a1f5c3e
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-02-18 11:54:57 +1300

    set up part of first test and added scripts to package.json

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var test = require('tape')
+
+
+
+var expectedOutput = 
+
+
+test("get request test", function (t) {
+
+	t.equal(functionName(), expectedOutput, "Expected output matches actual output")
+	t.end()
+})

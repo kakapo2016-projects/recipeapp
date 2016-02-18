6f35d02442efee8b1d2ee15d81c7801e5daf914e
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-18 17:25:27 +1300

    add first pass at express pass

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ var express = require('express')
 var fs = require('fs')
 var cors = require('cors')
 var path = require('path')
+var bodyParser = require('body-parser')
 var app = express()
+
+app.use(bodyParser.json());
 
 app.use(cors({
   origin: 'http://localhost:9966'
@@ -24,6 +27,33 @@ app.get('/fridge', function (req, res) {
       }
      })
   // res.send("send")
+})
+
+app.post('/addItemToFridge', function (req, res) {
+  console.log("in post", req.body)
+  var item = req.body.item
+  // hard code an item
+  //var item = '{"item":"tomatoes"}'
+  var thisPath = (__dirname + '/../data/db.json')
+  fs.readFile(thisPath, 'utf8', function(err, data) {
+    if (err) {
+      console.log("error", thisPath);
+    } else {
+       var itemsData = JSON.parse(data);        // convert to an array
+       var itemsArray = itemsData['fridge'];
+       itemsArray.push(JSON.parse(item));          // push the new item to the array of items
+       fs.writeFile(thisPath, JSON.stringify(itemsArray), 'utf8', function(err, data){
+         if (err){
+           console.log('error')
+         } else {
+           res.send('POST request to addItemToFridge')
+         }
+
+       })
+
+     }
+  })
+  // res.send(item)
 })
 
 app.listen(3000, function () {

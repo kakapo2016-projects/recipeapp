4106c224f9f4e813773538d08c47bad8afc6c4d7
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-02-18 14:10:28 +1300

    Added newItem variable

--- src/components/postRequest.js
+++ src/components/postRequest.js
@@ -1,14 +1,15 @@
 var request = require('superagent')
 
-var postRequest = function(callBack) {
+
+var postRequest = function(newItem, callBack) {
 
 	request
 		.post('http://localhost:3000/fridge')
-		.send(     )
+		.send(newItem)
 		.end(function(err, res) {
 			if (err) throw err;
 			console.log('Adding item')
-			console.log(JSON.stringify(res.body))
+			console.log(JSON.stringify(newItem))
 		}
 	)
 }

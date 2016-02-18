8c2dc9a2d77e0f694763ac7ee1f74df4a4e7a4ef
Ashley <Ashleystanbridge@icloul.com>
2016-02-19 10:47:05 +1300

    what

--- src/components/append.js
+++ src/components/append.js
@@ -2,7 +2,7 @@ var $ = require('jquery')
 
 module.exports = function(text, id){ 
   $(document).ready(function () {
-    $(id).append("<li>" + text + "</li>")
+    $(id).append("<input type='checkbox' vaule='temName'>" + text + "<br>")
   })
 }
 

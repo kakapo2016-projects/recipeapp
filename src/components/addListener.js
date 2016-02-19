3eb4837895132d34d4f81bece58eb9a0e4879993
gaynor-landmark <g.l.landmark@gmail.com>
2016-02-19 15:51:50 +1300

    delete from shopping list

--- src/components/addListener.js
+++ src/components/addListener.js
@@ -1,5 +1,6 @@
 var jquery = require('jquery') //will this conflict with cdn jquery
 var postRequest = require('./postRequest')
+var putRequest = require('./putRequest')
 var posturl = 'http://localhost:3000/addItemToFridge'
 module.exports = function(callback){
 
@@ -8,7 +9,29 @@ module.exports = function(callback){
 			console.log('click')
 			var newItem = {}
 			newItem["item"] = jquery('#foodItem').val()
-			newItem["location"] = "shop" 
+			newItem["location"] = "shop"
 			postRequest(posturl, newItem)
 		})
+		jquery('#food_list').delegate(".chkbx","change",(function(e) {
+
+		    if(this.checked) {
+		        //Do stuff
+						console.log("check thischanged", e.target.id)
+						var item = {}
+						item["ID"] = e.target.id
+						item["item"] = e.target.value
+						item["location"] = "pantry"
+						putRequest('http://localhost:3000/updateItem', item)
+
+		    }
+		}));
+		// jquery('#food_list').on("chkbx","change",(function(e) {
+		// 	console.log("checkbox")
+		// 	console.log( $( this ).text() );
+		// 		if(this.checked) {
+		// 				//Do stuff
+		// 				console.log("check thischanged")
+		// 		}
+		// }));
+
 	}
